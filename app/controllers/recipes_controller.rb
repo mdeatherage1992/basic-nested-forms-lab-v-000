0975a24dbc80d4e6847eddff3f58ea94074a95ff
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-10 13:28:19 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,36 @@
 class RecipesController < ApplicationController
-  def show
+  
+  def show 
     @recipe = Recipe.find(params[:id])
-  end
-
-  def index
-    @recipes = Recipe.all
+    if @recipe.ingredients.last.try(:name)
+      @recipe.ingredients.build
+    end
   end
 
   def new
-    @recipe = Recipe.new
+  @recipe = Recipe.new
+  @recipe.ingredients.build(ingredient_name_1: 'pepper')
+  @recipe.ingredients.build(ingredient_name_2: 'salt')
   end
 
-  def create
-  end
+def create
+  recipe = Recipe.create(recipe_params)
+  redirect_to recipe_path
+end
+
+def index
+  @recipes = Recipe.all
+end
+
+private
+
+def recipe_params
+  params.require(:recipe).permit(
+    :name,
+    ingredients_attributes: [
+      :ingredient_quantity,
+      :ingredient_name
+    ]
+  )
+end
 end

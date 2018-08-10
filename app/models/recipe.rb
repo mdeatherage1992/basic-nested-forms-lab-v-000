7ed12a39c395cbe4861b42bdddbeb320fd67fcea
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-10 13:16:39 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,18 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+
+  def new
+  @recipe = Recipe.new
+  @recipe.ingredients.build(ingredient_name: 'pepper')
+  @recipe.ingredients.build(ingredient_name: 'salt')
+  end
+
+def create
+  person = Person.create(person_params)
+  redirect_to people_path
+end
+
+def index
+  @people = Person.all
+end
 end
